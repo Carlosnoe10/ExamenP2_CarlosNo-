8407d7daf07897e9bec424a93e26d55db43541a2
Carlosnoe10 <carlosdnoem@gmail.com>
2024-03-22 13:03:36 -0700

    Primere Commit

---
+++ src/examenlp2_carlosnoé/Carro.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenlp2_carlosnoé;
+
+/**
+ *
+ * @author cd507
+ */
+public class Carro {
+    public String Marca;
+    public String Modelo;
+    public int Velocidad;
+
+    public Carro(String Marca, String Modelo, int Velocidad) {
+        this.Marca = Marca;
+        this.Modelo = Modelo;
+        this.Velocidad = Velocidad;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String Modelo) {
+        this.Modelo = Modelo;
+    }
+
+    public int getVelocidad() {
+        return Velocidad;
+    }
+
+    public void setVelocidad(int Velocidad) {
+        this.Velocidad = Velocidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" + "Marca=" + Marca + ", Modelo=" + Modelo + ", Velocidad=" + Velocidad + '}';
+    }
+    
+    
+    
+}
